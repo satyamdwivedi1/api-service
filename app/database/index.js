609a19b7404d9c438a9a2c89d334d4d8a3b5223d
p2d1-admin <satyamkumardwivedi1103@gmail.com>
2023-03-05 16:51:06 +0530

    Update index.js

--- app/database/index.js
+++ app/database/index.js
@@ -1,6 +1,5 @@
 var mongoClient = require("mongodb").MongoClient;
-var url =
-  process.env.MONGO_DB_URL || "mongodb+srv://Satyam123:TZjOPhtHgtTKHlNh@first-cluster.2i0ptnt.mongodb.net/?retryWrites=true&w=majority";
+var url = "mongodb+srv://Satyam123:TZjOPhtHgtTKHlNh@first-cluster.2i0ptnt.mongodb.net/?retryWrites=true&w=majority";
 const database = "Medical-Store";
 
 const client = new mongoClient(url);
