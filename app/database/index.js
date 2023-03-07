e9d116db33eea6d771b16132ecde0c8fdaa4f65b
p2d1-admin <satyamkumardwivedi1103@gmail.com>
2023-03-07 16:10:15 +0530

    update env

--- app/database/index.js
+++ app/database/index.js
@@ -1,10 +1,10 @@
 var mongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-var url = process.env.MONGO_URI;
+var url = "mongodb+srv://Satyam123:TZjOPhtHgtTKHlNh@first-cluster.2i0ptnt.mongodb.net/Medical-Store?retryWrites=true&w=majority";
 const database = "Medical-Store";
 
-const client = new mongoClient(url);
+const client = new mongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectDb() {
   let result = await client.connect();
