9823ea6736d42c8bca990e70a139e2c5d5c6a174
p2d1-admin <satyamkumardwivedi1103@gmail.com>
2023-03-19 03:13:51 +0530

    added crud for products

--- index.js
+++ index.js
@@ -42,6 +42,68 @@ app.get("/all-purchases/:type?", async (req, res) => {
   res.status(200).json(products);
 });
 
+app.get("/product/:_id", async (req, res) => {
+  let db = await connectDb();
+  let products = [];
+  if (req.params._id) {
+    products = await db
+      .collection("Products")
+      .find({ _id: ObjectId(req.params._id) })
+      .toArray();
+  } else {
+    return res.status(404).send({ Message: `Product with.${_id} is not available.` });
+  }
+  res.status(200).json({ product: products[0] });
+});
+
+app.post("/product", async (req, res) => {
+  let db = await connectDb();
+  let product;
+  const productDetails = req.body;
+  const isEmpty = Object.values(productDetails).every((x) => x === null || x === "");
+  if (productDetails && !isEmpty) {
+    product = await db.collection("Products").insertOne(productDetails);
+  } else {
+    return res.status(404).send({ Message: `Please provide product details.` });
+  }
+  res.status(200).send({ message: "Product added successfully", productId: product.insertedId });
+});
+
+app.put("/updateProduct/:_id", async (req, res) => {
+  let db = await connectDb();
+  let product;
+  if (req.params._id) {
+    const productDetails = req.body;
+    const isEmpty = Object.values(productDetails).every((x) => x === null || x === "");
+    if (isEmpty) {
+      return res.status(404).send({ Message: `Please provide product details.` });
+    } else {
+      product = await db.collection("Products").replaceOne({ _id: ObjectId(req.params._id) }, req.body);
+    }
+  } else {
+    return res.status(404).send({ Message: `Product with.${_id} is not available.` });
+  }
+  res.status(200).json({ message: "Product updated successfully" });
+});
+
+app.delete("/deleteProduct", async (req, res) => {
+  let db = await connectDb();
+  let product;
+  if (req.query.productId) {
+    if (typeof req.query.productId === "string") {
+      product = await db.collection("Products").deleteOne({ _id: ObjectId(req.query.productId) });
+    }
+    if (typeof req.query.productId === "object") {
+      const productsIds = req.query.productId.map((product) => ObjectId(product._id));
+      product = await db.collection("Products").remove({ _id: { $in: productsIds } });
+      console.log(product, productsIds);
+    }
+  } else {
+    return res.status(404).send({ Message: `Please provide the product id` });
+  }
+  res.status(200).json({ message: "Product deleted successfully" });
+});
+
 app.post("/auth/login", async (req, res) => {
   if (!req.body.username) return res.status(401).send({ Message: "Username is required." });
   if (req.body.username) {
